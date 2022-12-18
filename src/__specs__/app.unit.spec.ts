dcf04541b592234ec90ad88ccaa30837fbac05da
Trejon <Trejonstallsworth@gmail.com>
2022-12-18 16:22:24 -0700

    Finished the fetching logic, added tests and updated output style

--- src/__specs__/app.unit.spec.ts
+++ src/__specs__/app.unit.spec.ts
@@ -1,6 +1,10 @@
-import { validatePhotoAlbumId } from '../app';
+import axios from 'axios';
 
-describe('Validate photo album ids', () => {
+import { validatePhotoAlbumId, fetchAlbum } from '../app';
+
+jest.mock('axios');
+
+describe('Validating photo album ids', () => {
     test('Allows and returns a valid id', async () => {
         expect(validatePhotoAlbumId(2)).toEqual(2);
     });
@@ -11,5 +15,16 @@ describe('Validate photo album ids', () => {
 
     test("Returns false if id doesn't exist", async () => {
         expect(validatePhotoAlbumId('5001')).toEqual(false);
+    });
+});
+
+describe('Testing axios', () => {
+    it('Returns data for a valid id', async () => {
+        const logSpy = jest.spyOn(console, 'log');
+        (axios.get as jest.Mock).mockImplementationOnce(() => Promise.resolve({}));
+        const data = await fetchAlbum(3);
+        expect(axios.get).toHaveBeenCalled();
+        expect(data).not.toBeNull();
+        expect(logSpy).toBeCalled();
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -4,50 +4,45 @@ import axios from 'axios';
 import chalk from 'chalk';
 import boxen from 'boxen';
 
+import { AlbumResponse } from './types/types';
+
 const providedArg = process.argv.slice(2).toString();
 const photoId = parseInt(providedArg);
-
-export const validatePhotoAlbumId = (photoArg: number | string) => {
-    if (isNaN(photoArg as number)) {
-        console.log('Please provide a number for a valid photo album id.');
-        return false;
-    }
-
-    if (photoArg > 4999 || photoArg < 0) {
-        console.log('Please confirm id and try again.');
-        return false;
-    }
-
-    return photoArg;
-};
 
 const boxenOptions: Partial<boxen.Options> = {
     borderColor: 'green',
     backgroundColor: '#555555'
 };
 
-const colorOutput = (msg: string) => {
+const colorLog = (msg: string) => {
     console.log(boxen(chalk.white.bold(msg), boxenOptions));
 };
 
-type AlbumResponse = {
-    albumId: number;
-    id: number;
-    thumbnailUrl: string;
-    title: string;
-    url: string;
+export const validatePhotoAlbumId = (photoArg: number | string) => {
+    if (isNaN(photoArg as number)) {
+        colorLog('Please provide a number for a valid photo album id.');
+        return false;
+    }
+
+    if (photoArg > 4999 || photoArg < 0) {
+        colorLog('Please confirm id and try again.');
+        return false;
+    }
+
+    return photoArg;
 };
 
 export const fetchAlbum = async (albumId: number) => {
     const baseUrl = 'https://jsonplaceholder.typicode.com/photos';
     try {
         const { data } = await axios.get(`${baseUrl}?albumId=${albumId}`);
-        data.forEach((res: AlbumResponse) => colorOutput(`[${res.id}] ${res.title}`));
+        if (data.length < 1) console.log('Sorry, there is nothing in this album.');
+        return data.forEach((res: AlbumResponse) => colorLog(`[${res.id}] ${res.title}`));
     } catch (err) {
         if (axios.isAxiosError(err)) {
-            colorOutput('There was an issue with axios.');
+            return colorLog('There was an issue with axios...');
         } else {
-            colorOutput('There seems to be an issue requesting that data...');
+            return colorLog('There seems to be an issue requesting that data...');
         }
     }
 };
