4dea4dfd33d1e40c2f44440c1536e0a2f374b33c
Trejon <Trejonstallsworth@gmail.com>
2022-12-18 11:38:04 -0700

    wrote logic to pass validatePhotoAlbumId tests

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,16 @@ const providedArg = process.argv.slice(2).toString()
 const photoId = parseInt(providedArg)
 console.log(photoId)
 
-export const validatePhotoAlbumId = (photoArg: string | number) => {
-    return true
+export const validatePhotoAlbumId = (photoArg: number | string) => {
+    if (isNaN(photoArg as number)) {
+        console.log('Please provide a number for a valid photo album id.')
+        return false
+    }
+
+    if (photoArg > 4999 || photoArg < 0) {
+        console.log('Please confirm id and try again.')
+        return false
+    }
+
+    return photoArg
 }
