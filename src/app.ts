7d4ba1e0d0f2acf53e9bc58f64a8c64c6a381115
Trejon <Trejonstallsworth@gmail.com>
2022-12-18 11:30:19 -0700

    changed output dir

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-const providedArg = process.argv.slice(2).toString();
-const photoId = parseInt(providedArg);
+const providedArg = process.argv.slice(2).toString()
+const photoId = parseInt(providedArg)
+console.log(photoId)
 
 export const validatePhotoAlbumId = (photoArg: string | number) => {
-  return true;
-};
+    return true
+}
